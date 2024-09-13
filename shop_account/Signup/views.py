05429249a72795ea7528efe675f7445534750fb0
Sivam <shiva4523e@gmail.com>
2024-09-14 05:15:17 +0530

    email sending done

--- shop_account/Signup/views.py
+++ shop_account/Signup/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def signup(request):
+    if request.method == 'POST':
+        message = "Hello"
+        email = request.POST.get('email')
+        name = "Sivam"
+        send_mail(
+            'contact from',
+            message,
+            'settings.EMAIL_HOST_USER',
+            [email], 
+            fail_silently=False)
+        return render(request, 'home.html')
+    else:
+        print("HAHAHA")
+        
     return render(request, 'signup.html')
